80d19f8d40b16bc634c49c054b43afcc87fe1c41
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-02-25 18:30:50 +0000

    Initial work on wrapper

---
+++ src/hankel_transform.c
@@ -0,0 +1,122 @@
+#include "libhankel.h"
+#include <string.h>
+
+
+typedef struct {
+    double n_eval;      // integer indicating number of function evaluations (N_ogata in SASfit)
+    double eps_rel;     // relative error allowed e.g. 1e-9 (eps_nriq in SASfit)
+    double f_max;       // float indicating starting guess for max in form factor (h_ogata in SASfit)
+} strategy_params;
+
+
+/** 
+ * @brief Computes Hankel transform using the method specified by the user.
+ * @note The parameter strategy_name can be any of "QWE_Chave", "QWE_Key", 
+ *       "DHT_6", "DHT_7", "DHT_8", "DHT_9", "DHT_10", "DHT_11".
+ * 
+ * @param nu               order of bessel function - must be 0 or 1
+ * @param f                pointer to form factor function
+ * @param x                value at which to compute the transform
+ * @param fparams          params for form factor
+ * @param output           pointer to var containing output from transform 
+ * @param strategy_name    str corresponding to the strategy name
+ * @param strategy_params  struct containing params for the specific strategy
+ *                         (please use the docs to find params required by each strategy)
+ */ 
+double hankel_transform(
+    int nu, 
+    double (*f)(double, double (*)[50]), 
+    double x, 
+    double (*fparams)[50], 
+    double *output,
+    char strategy_name,
+    const void* strategy_params) {
+
+    int status;
+
+    if (strcmp(strategy_name, "QWE_Chave") == 0) {
+        status = hankel_transform_QWE_Chave(
+            nu, 
+            f, 
+            x, 
+            fparams,
+            output, 
+            strategy_params.n_eval, 
+            strategy_params.eps_rel
+        )
+
+    } elif (strcmp(strategy_name, "QWE_Key") == 0) {
+        status = hankel_transform_QWE_Key(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            strategy_params.n_eval, 
+            strategy_params.eps_rel
+        )
+
+    } elif (strcmp(strategy_name, "DHT_6") == 0) {
+        status = hankel_transform_DHT(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            6
+        )
+
+    } elif (strcmp(strategy_name, "DHT_7") == 0) {
+        status = hankel_transform_DHT(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            7
+        )
+
+    } elif (strcmp(strategy_name, "DHT_8") == 0) {
+        status = hankel_transform_DHT(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            8
+        )
+
+    } elif (strcmp(strategy_name, "DHT_9") == 0) {
+        status = hankel_transform_DHT(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            9
+        )
+
+    } elif (strcmp(strategy_name, "DHT_10") == 0) {
+        status = hankel_transform_DHT(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            10
+        )
+
+    } elif (strcmp(strategy_name, "DHT_11") == 0) {
+        status = hankel_transform_DHT(
+            nu, 
+            f, 
+            x, 
+            fparams, 
+            output,
+            11
+        )
+    }
+
+    return status;
+    
+}
