ecac4ac72d37aee4eb6fc0e2da592a6350acdc3f
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-04-23 15:38:43 +0100

    Get Python API to work and add example

--- examples/example_usage_python.py
+++ examples/example_usage_python.py
@@ -1,4 +1,25 @@
-
+import numpy as np
 import libhankel
 
-print(libhankel.form_factor_g_dab(1.0, [10.0, 0.5, 1.0, 1e-4]))
+nu = 0
+params_spheres = np.array([10.0,  1.0])
+x_arr = np.array([1.0,  2.0,  3.0,  4.0,  5.0,  6.0])
+
+strategy_p_dict = {
+    'n_eval': 250, 
+    'eps_rel': 1e-9
+}
+
+result = libhankel.hankel_transform(
+    nu,
+    libhankel.form_factor_sphere_callback,
+    x_arr,
+    params_spheres,
+    "QWE_Chave",
+    strategy_p_dict
+)
+
+print(
+    "Result of calling the hankel_transform with sphere form factor", 
+    result
+)
--- libhankel/_clib.py
+++ libhankel/_clib.py
@@ -5,54 +5,69 @@ from ctypes import (
     POINTER, 
     CFUNCTYPE, 
     c_char_p,
-    Structure
+    Structure,
+    c_size_t
 )
 
-libhankel = CDLL("liblibhankel.so")
+libhankel = CDLL("build/src/liblibhankel.so")
 
-
-###############################################
-# For hankel_transform function
-###############################################
 Array50 = c_double * 50
 PtrToArray50 = POINTER(Array50)
-
-FUNC_TYPE = CFUNCTYPE(
+FORM_FACTOR_FUNC_TYPE = CFUNCTYPE(
     c_double,      # return type
     c_double,      # first arg
     PtrToArray50   # second arg: double (*)[50]
 )
 
-class strategy_params(Structure):
+class StrategyParams(Structure):
     _fields_ = [
-        ("n_eval", c_double),
+        ("n_eval", c_int),
         ("eps_rel", c_double),
-        ("f_max", c_double)
+        ("f_max", c_double),
     ]
 
-# libhankel.hankel_transform.argtypes = [
-#     c_int,
-#     FUNC_TYPE, 
-#     POINTER(c_double),
-#     PtrToArray50,
-#     POINTER(c_double),
-#     c_int,
-#     c_char_p,
-#     strategy_params
-# ]
 
-# libhankel.hankel_transform.restype = c_int
+libhankel.hankel_transform.argtypes = [
+    c_int,
+    FORM_FACTOR_FUNC_TYPE, 
+    POINTER(c_double),
+    PtrToArray50,
+    POINTER(c_double),
+    c_size_t,
+    c_char_p,
+    StrategyParams
+]
+libhankel.hankel_transform.restype = c_int
 
 
-###############################################
-# For form_factor_g_dab function
-###############################################
 libhankel.form_factor_g_dab.argtypes = [
     c_double,       # first arg
     PtrToArray50    # second arg: double (*)[50]
 ]
 libhankel.form_factor_g_dab.restype = c_double
 
+libhankel.form_factor_broad_peak.argtypes = [
+    c_double,       # first arg
+    PtrToArray50    # second arg: double (*)[50]
+]
+libhankel.form_factor_broad_peak.restype = c_double
 
-###############################################
-###############################################
+libhankel.form_factor_sphere.argtypes = [
+    c_double,       # first arg
+    PtrToArray50    # second arg: double (*)[50]
+]
+libhankel.form_factor_sphere.restype = c_double
+
+
+form_factor_g_dab_callback = FORM_FACTOR_FUNC_TYPE(
+    ("form_factor_g_dab", libhankel)
+)
+form_factor_broad_peak_callback = FORM_FACTOR_FUNC_TYPE(
+    ("form_factor_broad_peak", libhankel)
+)
+form_factor_sphere_callback = FORM_FACTOR_FUNC_TYPE(
+    ("form_factor_sphere", libhankel)
+)
+
+
+
