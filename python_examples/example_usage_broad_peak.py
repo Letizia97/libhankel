d68cda2dfceb69eafd4e9eb847ad75c38910dc18
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-04-23 15:58:11 +0100

    Add example usages for all form factors

---
+++ python_examples/example_usage_broad_peak.py
@@ -0,0 +1,29 @@
+import numpy as np
+import libhankel
+
+nu = 0
+params_broad_peak = np.array([10e5, 1000, 0.01, 2, 2])
+x_arr = np.array([
+    30.,   49.,   69.,   88.,   108.,  127.,  147.,  167.,  186.,  206.,  225., 
+    225.,  245.,  265.,  284.,  304.,  323.,  343.,  362.,  382.,  402.,  421.,  
+    441.,  460.,  480.,         
+])
+
+strategy_p_dict = {
+    'n_eval': 250, 
+    'eps_rel': 1e-9
+}
+
+result = libhankel.hankel_transform(
+    nu,
+    libhankel.form_factor_broad_peak_callback,
+    x_arr,
+    params_broad_peak,
+    "QWE_Chave",
+    strategy_p_dict
+)
+
+print(
+    "Result of calling the hankel_transform with broad_peak form factor", 
+    result
+)
---
+++ python_examples/example_usage_g_dab.py
@@ -0,0 +1,31 @@
+import numpy as np
+import libhankel
+
+nu = 0
+params_gdab = np.array([10.0,  0.5, 1e-4])
+x_arr = np.array([
+    15.,          18.54166667,  22.08333333,  25.625,       29.16666667,
+    32.70833333,  36.25,        39.79166667,  43.33333333,  46.875,
+    50.41666667,  53.95833333,  57.5,         61.04166667,  64.58333333,
+    68.125,       71.66666667,  75.20833333,  78.75,        82.29166667,
+    85.83333333,  89.375,       92.91666667,  96.45833333,  100.,   
+])
+
+strategy_p_dict = {
+    'n_eval': 250, 
+    'eps_rel': 1e-9
+}
+
+result = libhankel.hankel_transform(
+    nu,
+    libhankel.form_factor_g_dab_callback,
+    x_arr,
+    params_gdab,
+    "QWE_Chave",
+    strategy_p_dict
+)
+
+print(
+    "Result of calling the hankel_transform with g_dab form factor", 
+    result
+)
