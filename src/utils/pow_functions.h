7665c52736e02e84262554e25d2de9e11f34b842
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-05-20 16:15:13 +0100

    Replace gsl_pow calls

---
+++ src/utils/pow_functions.h
@@ -0,0 +1,17 @@
+
+#ifndef POW_FUNCTIONS_H
+#define POW_FUNCTIONS_H
+
+static inline double pow2(double x) { return x * x; }
+
+static inline double pow3(double x) { return x * x * x; }
+
+static inline double pow4(double x) { return x * x * x * x; }
+
+static inline double pow5(double x) { return x * x * x * x * x; }
+
+static inline double pow6(double x) { return x * x * x * x * x * x; }
+
+static inline double pow7(double x) { return x * x * x * x * x * x * x; }
+
+#endif // POW_FUNCTIONS_H
